ccc359420eec52eac89d193fd434153d97060121
grandmaster25 <agams25@gmail.com>
2017-09-30 01:21:15 +0530

    add 'nice to have' message
    Signed-off-by: grandmaster25 <agams25@gmail.com>

--- file01.js
+++ file01.js
@@ -3,6 +3,7 @@ function greet(name)
     console.log('knock knock knock ' + name)
     console.log('knock knock knock ' + name)
     console.log('knock knock knock ' + name)
+    console.log('Good to have you here!')
 
 }
 greet('agam')
